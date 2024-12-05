5c1198e62c4ac9561406f19466e3afdf6dc36416
Brandon Walsh <bmwalshy@gmail.com>
2024-12-05 15:42:22 -0500

    add instructions

--- day2/script.ts
+++ day2/script.ts
@@ -28,7 +28,6 @@ async function part1(): Promise<void> {
       previous = current;
     }
 
-    //console.log("safe", line);
     safe += 1;
   }
   // Read input
@@ -38,3 +37,44 @@ async function part1(): Promise<void> {
 }
 
 part1();
+
+async function part2(): Promise<void> {
+  let safe = 0;
+
+  function processLine(line: string): void {
+    let badLevels: number = 0;
+    const nums: string[] = line.split(" ");
+    const first: number = parseInt(nums[0]);
+    let previous: number = parseInt(nums[1]);
+    const increasing: boolean = first < previous;
+
+    if (first == previous) badLevels += 1;
+    if (Math.abs(first - previous) > 3) badLevels += 1;
+
+    for (let i = 2; i < nums.length; i++) {
+      if (badLevels >= 2) return;
+
+      const current = parseInt(nums[i]);
+
+      if (Math.abs(current - previous) > 3) badLevels += 1;
+
+      if (
+        (increasing && current < previous) ||
+        (!increasing && current > previous)
+      )
+        badLevels += 1;
+
+      if (current == previous) badLevels += 1;
+
+      previous = current;
+    }
+
+    if (badLevels <= 1) safe += 1;
+  }
+  // Read input
+  await processInput(`${__dirname}/input.txt`, processLine);
+
+  console.log(`Part 1: ${safe}`);
+}
+
+part2();
