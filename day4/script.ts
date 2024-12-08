ba1792ee4248c09cc9353282297009a3ee2ff387
Brandon Walsh <bmwalshy@gmail.com>
2024-12-08 11:13:42 -0500

    day 4 pt 1

--- day4/script.ts
+++ day4/script.ts
@@ -1,52 +1,121 @@
 import { processInput } from "../common/utils";
 
 async function part1(): Promise<void> {
-  const grid: Node[][] = [];
-  const j = 0;
+  const grid: string[][] = [];
 
   function processLine(line: string): void {
-    const chars: string[] = line.split("");
+    grid.push(line.split(""));
   }
 
   await processInput(`${__dirname}/input.txt`, processLine);
 
-  function dfs() {}
+  const nodes: Node[] = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const node: Node = {
+        val: grid[i][j],
+        i: i,
+        j: j,
+        running: grid[i][j],
+        direction: [0, 0],
+      };
+      nodes.push(node);
+    }
+  }
+
+  function addNewNode(
+    currentRow: number,
+    currentCol: number,
+    rowDirection: number,
+    colDirection: number,
+    running: string,
+  ) {
+    const nextRow = currentRow + rowDirection;
+    const nextCol = currentCol + colDirection;
+    if (
+      nextRow >= grid.length ||
+      nextRow < 0 ||
+      nextCol >= grid[0].length ||
+      nextCol < 0
+    ) {
+      return;
+    }
+
+    const nextChar: string = grid[nextRow][nextCol];
+
+    if (!valid.has(running + nextChar)) {
+      return;
+    }
+
+    nodes.push({
+      val: nextChar,
+      i: nextRow,
+      j: nextCol,
+      running: running + nextChar,
+      direction: [rowDirection, colDirection],
+    });
+  }
+
+  let xmasCount = 0;
+  const seen: Set<string> = new Set();
+  const valid: Set<string> = new Set(["X", "XM", "XMA", "XMAS"]);
+
+  // dfs dfs dfs dfs!!
+  while (nodes.length > 0) {
+    // @ts-ignore
+    const current: Node = nodes.pop();
+    const currentRow: number = current.i;
+    const currentCol: number = current.j;
+    const running: string = current.running;
+    const direction = current.direction;
+    const seenIndex = `${currentRow},${currentCol},${direction.toString()}`;
+
+    if (seen.has(seenIndex)) {
+      continue;
+    }
+
+    seen.add(seenIndex);
+
+    if (running.length >= 4) {
+      if (current.running === "XMAS") {
+        xmasCount++;
+      }
+      continue;
+    }
+
+    if (!valid.has(running)) {
+      seen.add(seenIndex);
+      continue;
+    }
+
+    seen.add(seenIndex);
+
+    if (!(direction[0] == 0 && direction[1] == 0)) {
+      addNewNode(currentRow, currentCol, direction[0], direction[1], running);
+      continue;
+    }
+
+    // Check surrounding neighbors
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        if (i == 0 && j == 0) {
+          continue;
+        }
+        addNewNode(currentRow, currentCol, i, j, running);
+      }
+    }
+  }
+
+  console.log(`Part 1: ${xmasCount}`);
 }
 
-part1();
-
-type NodeParams = {
-  tl: Node | null;
-  tm: Node | null;
-  tr: Node | null;
-  l: Node | null;
-  r: Node | null;
-  bl: Node | null;
-  bm: Node | null;
-  br: Node | null;
+type Node = {
   val: string | null;
+  i: number;
+  j: number;
+  running: string;
+  direction: number[];
 };
 
-class Node {
-  topLeft: Node | null = null;
-  topMid: Node | null = null;
-  topRight: Node | null = null;
-  left: Node | null = null;
-  right: Node | null = null;
-  botLeft: Node | null = null;
-  botMid: Node | null = null;
-  botRight: Node | null = null;
-
-  val: null | string;
-
-  constructor(params: NodeParams) {
-    this.topLeft = params.tl;
-    this.topMid = params.tm;
-    this.topRight = params.tr;
-    this.left = params.l;
-    this.right = params.r;
-    this.botLeft = params.bl;
-    this.botMid = params.bm;
-    this.botRight = params.br;
-  }
-}
+part1();
